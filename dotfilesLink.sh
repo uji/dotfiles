4d0e6e7a4d21ed1f4591dc4253d11a6146fcf73d
uji <ujiprog@gmail.com>
2020-03-15 14:39:27 +0900

    rm dotrilesLink.sh

--- dotfilesLink.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-ln -sf ~/dotfiles/vimrc/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/vimrc/.vim ~/.vim
-
-ln -sf ~/dotfiles/vimrc/.vim ~/.config/nvim
-ln -sf ~/dotfiles/vimrc/.vimrc ~/.config/nvim/init.vim
