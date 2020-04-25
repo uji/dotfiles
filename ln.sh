f4810209b0515b311651de69465626b32f86c3fc
uji <ujiprog@gmail.com>
2020-04-25 23:22:14 +0900

    rename .vim -> vim

--- ln.sh
+++ ln.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 mkdir -p ~/.config
 ln -sf ~/dotfiles/vimrc/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/vimrc/.vim ~/.vim
-ln -sf ~/dotfiles/vimrc/.vim ~/.config/nvim
+ln -sf ~/dotfiles/vimrc/vim ~/.vim
+ln -sf ~/dotfiles/vimrc/vim ~/.config/nvim
 ln -sf ~/dotfiles/vimrc/.vimrc ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
