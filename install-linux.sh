a10c2873f9409fc2e4fe84bbf8d4c1e37cb06aa1
uji <ujiprog@gmail.com>
2020-07-02 01:31:06 +0900

    feat: install vim & ag by apt-get

--- install-linux.sh
+++ install-linux.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+if type "apt-get" > /dev/null 2>&1; then
+  apt-get update
+  apt-get install -y vim silversearcher-ag
+fi
+
 sh ~/dotfiles/common.sh
 sh ~/dotfiles/ln.sh
 sh ~/dotfiles/install-vimpack.sh
