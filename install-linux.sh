584526dacd7010b4e51a1367ffc5af7fcfb1afad
uji <ujiprog@gmail.com>
2020-07-16 02:15:03 +0900

    fix: fix install-vim script

--- install-linux.sh
+++ install-linux.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 if type "apt-get" > /dev/null 2>&1; then
   apt-get update
-  apt-get install -y vim silversearcher-ag
+  apt-get install -y silversearcher-ag
 fi
 
+sh ~/dotfiles/install-vim.sh
 sh ~/dotfiles/common.sh
 sh ~/dotfiles/ln.sh
 sh ~/dotfiles/install-vimpack.sh
--- install-vim.sh
+++ install-vim.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
+rm -rf ~/tmp/vim
 git clone https://github.com/vim/vim.git ~/tmp/vim
 cd ~/tmp/vim && ./configure \
   --prefix=/usr/local \
   --enable-multibyte \
   --enable-nls \
-  --enable-perlinterp \
-  --enable-python3interp --with-python3-command=`which python3` \
-  --enable-luainterp --with-lua-prefix=/usr/local \
   --enable-cscope \
-  --enable-fail-if-missing=yes \
-  --with-features=huge \
-  --with-luajit \
   --without-x \
   --disable-xim \
   --disable-gui \
   --disable-sysmouse \
   --disable-netbeans \
   --disable-xsmp \
-  CC=`which clang` CFLAG='-I/usr/local/include' \
   && make install
