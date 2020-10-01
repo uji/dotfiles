3c058f6704021dc835454137d4480e634553185c
uji <ujiprog@gmail.com>
2020-10-01 17:09:55 +0000

    fix: install fzf binary for vim in shellscript

--- install-vimpack.sh
+++ install-vimpack.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-vim -c 'call minpac#clean()' -c 'call minpac#update("", {"do": "quit"})' -c ':q'
+vim -c 'call minpac#clean()' \
+    -c 'call minpac#update("", {"do": "quit"})' \
+    -c 'call fzf#install()' \
+    -c ':q'
