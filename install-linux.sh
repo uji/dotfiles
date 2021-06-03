4e59d0ed83c4971b16338df475f9f2846f0aebbe
uji <49834542+uji@users.noreply.github.com>
2021-06-03 22:40:49 +0900

    fix: use ujivim by install-linux.sh

--- install-linux.sh
+++ install-linux.sh
@@ -4,7 +4,14 @@ if type "apt-get" > /dev/null 2>&1; then
   apt-get install -y libncurses5-dev silversearcher-ag
 fi
 
-sh ~/dotfiles/install-vim.sh
+curl -s https://api.github.com/repos/uji/ujivim/releases/latest \
+| grep "browser_download_url.*linux-amd64.tar.gz" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| xargs curl -o ~/ujivim.tar.gz -L
+
+tar -zxvf ~/ujivim.tar.gz
+
 sh ~/dotfiles/common.sh
 sh ~/dotfiles/ln.sh
 sh ~/dotfiles/install-vimpack.sh
