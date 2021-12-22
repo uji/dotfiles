adec6b4e9210fe24c0c942449e97cb93eb270cf2
uji <ujiprog@gmail.com>
2021-12-22 14:01:29 +0900

    fix: move git editor setting

--- git-setting.sh
+++ git-setting.sh
@@ -1,4 +1,3 @@
 git config --global user.name ${GITHUB_USER_NAME}
 git config --global user.email ${GITHUB_EMAIL}
 git config --global url."https://${GITHUB_USER_NAME}:${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
-git config --global core.editor vim
--- install.sh
+++ install.sh
@@ -1,27 +1,23 @@
 #!/bin/sh
 set -e
 
-use_package_manager() {
-  if type "brew" > /dev/null 2>&1; then
-    brew install vim
-    brew install tmux
-  elif type "apt-get" > /dev/null 2>&1; then
-    curl -s https://api.github.com/repos/neovim/neovim/releases/latest \
-    | grep "browser_download_url.*nvim.appimage\"" \
-    | cut -d : -f 2,3 \
-    | tr -d \" \
-    | xargs curl -o ~/nvim.appimage -L
-    chmod u+x ~/nvim.appimage
-    ~/nvim.appimage --appimage-extract
-    echo export PATH=$PATH:$HOME/squashfs-root/usr/bin >> ~/.bash_local
-  else
-    echo "Your os is not supported."
-  fi
-}
-
 touch ~/.bash_local
 
-use_package_manager
+if type "brew" > /dev/null 2>&1; then
+  brew install vim
+  brew install tmux
+elif type "apt-get" > /dev/null 2>&1; then
+  curl -s https://api.github.com/repos/neovim/neovim/releases/latest \
+  | grep "browser_download_url.*nvim.appimage\"" \
+  | cut -d : -f 2,3 \
+  | tr -d \" \
+  | xargs curl -o ~/nvim.appimage -L
+  chmod u+x ~/nvim.appimage
+  ~/nvim.appimage --appimage-extract
+  echo export PATH=$PATH:$HOME/squashfs-root/usr/bin >> ~/.bash_local
+else
+  echo "Your os is not supported."
+fi
 
 git clone https://github.com/uji/vimrc.git ~/dotfiles/vimrc
 sh ~/dotfiles/vimrc/install.sh
@@ -33,14 +29,15 @@ ln -sf ~/dotfiles/vimrc/vim ~/.config/nvim
 ln -sf ~/dotfiles/vimrc/.vimrc ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-
 . ~/.bashrc
 
 if type "nvim" > /dev/null 2>&1; then
+  git config --global core.editor nvim
   nvim -c 'call minpac#clean()' \
       -c 'call minpac#update("", {"do": "quit"})' \
       -c ':q'
 else
+  git config --global core.editor vim
   vim -c 'call minpac#clean()' \
       -c 'call minpac#update("", {"do": "quit"})' \
       -c ':q'
