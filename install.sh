88147d68f84798ba0c0db8a6c09ab0a34c9827f6
uji <49834542+uji@users.noreply.github.com>
2022-02-03 17:05:46 +0900

    fix: install.sh for zsh

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ ln -sf ~/dotfiles/vimrc/vim ~/.vim
 ln -sf ~/dotfiles/vimrc/vim ~/.config/nvim
 ln -sf ~/dotfiles/vimrc/.vimrc ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.bashrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 . ~/.bashrc
 
