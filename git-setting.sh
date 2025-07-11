703c92b5058602c7a1c22d5209591a182ed8f024
uji <ujiprog@gmail.com>
2025-07-12 02:18:14 +0900

    rm git-setting.sh

--- git-setting.sh
+++
@@ -1,3 +0,0 @@
-git config --global user.name ${GITHUB_USER_NAME}
-git config --global user.email ${GITHUB_EMAIL}
-git config --global url."https://${GITHUB_USER_NAME}:${GITHUB_TOKEN}@github.com/".insteadOf "https://github.com/"
